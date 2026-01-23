548c6801057d3803483df36535d7a3e6dc45df54
svetlana <levkovskayase@gmail.com>
2026-01-24 00:55:49 +0300

    fix: test styles

--- src/components/TimelineBlock/TimelineCircle.tsx
+++ src/components/TimelineBlock/TimelineCircle.tsx
@@ -18,7 +18,7 @@ export const TimelineCircle = forwardRef<SVGSVGElement, Props>(
     const hoverNumberRef = useRef<SVGTextElement | null>(null);
 
     const CIRCLE_SIZE = 530;
-    const PADDING = 70;
+    const PADDING = 75;
     const SVG_SIZE = CIRCLE_SIZE + PADDING * 2; // 770
     const CENTER = SVG_SIZE / 2;
     const RADIUS = CIRCLE_SIZE / 2;
